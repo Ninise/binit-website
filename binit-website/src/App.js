7fd00a5f14dd38a5915b37ba6cef40acf80d9d95
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-09-09 20:02:16 -0400

    added header

--- binit-website/src/App.js
+++ binit-website/src/App.js
@@ -1,33 +1,11 @@
 import React from 'react';
 import './App.css';
-// import { About } from './components/about/About';
 import { Header } from './components/header/Header';
-// import { Home } from './components/home/Home';
-// import { Skills } from './components/skills/Skills';
-// import { Services } from './components/services/Services';
-// import { Qualification } from './components/qualification/Qualification';
-// import { Testimonials } from './components/testimotions/Testimonial';
-// import { Contact } from './components/contact/Contact';
-// import { Footer } from './components/footer/Footer';
-// import { ScrollUp } from './components/scrollup/ScrollUp';
-// import { Work } from './components/work/Work';
 
 export const App = () => {
   return (
     <div className='main'>
       <Header />
-      {/* <main className='main'>
-        <Home />
-        <About />
-        <Skills />
-        <Services />
-        <Qualification />
-        <Work />
-        <Testimonials />
-        <Contact />
-      </main>
-      <Footer />
-      <ScrollUp /> */}
     </div>
   );
 };
